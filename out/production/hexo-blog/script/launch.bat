7242546b50bd9dd2c25decd56deea6ac2c48f604
CJ-xchina <1484895345@qq.com>
2023-09-22 20:18:48 +0800

    Initial commit

--- out/production/hexo-blog/script/launch.bat
+++ out/production/hexo-blog/script/launch.bat
@@ -4,9 +4,12 @@ chcp 65001 > nul
 
 set LOG_FILE=log.txt
 for /f "tokens=1-6 delims=/: " %%a in ("%DATE% %TIME%") do (
-    set TIMESTAMP=%%b年-%%c月-%%d日-%%a  %%e：%%f
+    set TIMESTAMP=%%b年-%%c月-%%d日-%%a %%e：%%f
 )
-call upload_code.bat
+
+for /L %%i in (1,1,3) do (
+    call upload_code.bat
+)
+
 echo [%TIMESTAMP%] 批处理程序开始执行 >> %LOG_FILE%
 (hexo algolia && hexo clean && hexo g -d) > temp.txt || call detect.bat
-
