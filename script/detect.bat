7ec7e8547036e7d320ab6b881f051c95766eb4af
CJ-xchina <1484895345@qq.com>
2023-10-05 17:10:34 +0800

    Initial commit

--- script/detect.bat
+++ script/detect.bat
@@ -15,4 +15,10 @@ echo [%TIMESTAMP%] 程序出现问题！！！！！！！！！！！ >> %LOG_F
 echo FATAL: Something's wrong. Please check the program.
 msg * FATAL: Something's wrong. Please check the program.
 
+echo To: 1484895345@qq.com > email.txt
+echo Subject: Test Email >> email.txt
+echo. >> email.txt
+echo 我是崔杰翔 >> email.txt
+powershell -ExecutionPolicy ByPass -command "Send-MailMessage -To '1484895345@qq.com' -From 'cuijiexiang23@mails.ucas.ac.cn' -Subject 'Test Email' -Body (Get-Content email.txt | Out-String) -SmtpServer 'mail.cstnet.cn'"
+del email.txt
 del temp.txt
