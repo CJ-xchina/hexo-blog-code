6a0e05cb1921d898d6df898ecebeba82ab374ce9
CJ-xchina <1484895345@qq.com>
2023-10-06 13:11:22 +0800

    Initial commit

--- out/production/hexo-blog/script/mail.bat
+++ out/production/hexo-blog/script/mail.bat
@@ -1,7 +1,7 @@
 ::chcp 65001 设置编码，防止中文乱码
 ::最后加入 pause 可显示窗口用于排查问题，可删除
 chcp 65001
-setlocal enabledelayedexpansion
+
 rem ----延时执行----
 @echo off
 f:
@@ -12,8 +12,7 @@ rem ----邮件主题----
 set s="Hexo 笔记上传出错"
 rem ----邮件内容-----
 
-set body=
-for /f "tokens=* delims=" %%i in (temp.txt) do set body=!body! %%i
+set body="Hexo upload failed"
 rem ----收件邮箱-----
 set t=cuijiexiang23@mails.ucas.ac.cn
 rem ----发件邮箱-----
@@ -22,5 +21,5 @@ rem ----邮箱秘钥-----
 set pw=KXQPCRAIVTLEGSMZ
 rem ----执行发送-----
 
-blat -body %body% -s %s% -t %t%  -charset Gb2312 -server smtp.163.com -f %f% -u %f% -pw %pw%
+blat -body %body% -s %s% -t %t% -charset Gb2312 -server smtp.163.com -f %f% -u %f% -pw %pw% -attach temp.txt
 ::pause
